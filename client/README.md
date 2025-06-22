5bc0f1e783526ae97180b0357db44c26226a7066
dabu76 <whghtjd45@gmail.com>
2025-06-22 19:45:57 +0900

    設計追加

--- client/README.md
+++ client/README.md
@@ -1,12 +1,7 @@
-# React + Vite
+■ TODO の登録処理について
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+- ユーザーがフォームに内容と予定日を入力し、登録ボタンを押すと、
+  React から ASP.NET API へ POST リクエストが送信される。
+- API は受け取ったデータを元に、データベースの`todos`テーブルに新規登録を行う。
+  この際、ID は自動採番され、状態は初期値「未完了」、作成日時・更新日時はサーバー時刻が設定される。
+- 登録されたデータは API レスポンスとしてフロントエンドに返却され、即時表示される。
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,107 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+//カレンダーライブラリ
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ja } from "date-fns/locale";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //カレンダーライブラリ変数
+  const [content, setContent] = useState("");
+  const [status, setStatus] = useState("incomplete");
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [todos, setTodos] = useState([]);
+  const nextId = useRef(1);
 
-  return <></>;
+  const handleDelete = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+
+    setTodos(newTodos);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: nextId.current,
+      content,
+      status,
+      selectedDate,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    nextId.current += 1;
+
+    setTodos([...todos, newTodo]);
+    setContent("");
+    setSelectedDate(null);
+  };
+  return (
+    <div className="todo-container">
+      <h1> TODOアプリ</h1>
+
+      {/* TODO登録フォーム */}
+      <form onSubmit={handleSubmit} className="todo-form">
+        <input
+          type="text"
+          placeholder="内容（全角100文字以内）"
+          maxLength={100}
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        {/*カスタムカレンダー */}
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          locale={ja}
+          dateFormat="yyyy年MM月dd日（eee）"
+          placeholderText="予定日を選択してください"
+        />
+
+        <button type="submit">登録</button>
+      </form>
+
+      {/* フィルター切り替えボタン */}
+      <div className="filter-buttons">
+        <button>全件</button>
+        <button>未完了のみ</button>
+        <button>完了のみ</button>
+      </div>
+
+      {/* TODO一覧表示 */}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <p>内容: {todo.content}</p>
+            <p>状態: {todo.status === "complete" ? "完了" : "未完了"}</p>
+            <p>
+              予定日:
+              {todo.selectedDate &&
+                new Intl.DateTimeFormat("ja-JP", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  weekday: "short",
+                }).format(todo.selectedDate)}
+              <div>
+                <button className="todo-Btn">完了</button>
+                <button className="todo-Btn modify">修正</button>
+                <button
+                  className="todo-Btn del"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  削除
+                </button>
+              </div>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
