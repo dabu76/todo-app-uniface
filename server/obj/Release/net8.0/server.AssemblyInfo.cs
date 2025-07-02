f3f662aa30fab8a9cd5cfdc4179d531ef920bd83
dabu76 <whghtjd45@gmail.com>
2025-07-02 21:22:22 +0900

    エラーメッセージの定数化とコンポーネント分離

--- server/obj/Release/net8.0/server.AssemblyInfo.cs
+++ server/obj/Release/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de809a898a0c960b66c86577ebbbb656866c57a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64cc075298e65954e2431774b7b6717189a0dc40")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
